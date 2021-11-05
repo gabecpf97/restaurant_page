0212535e55c5c3efe6c9dd8a20a4ceb5538dee57
gabecpf97 <gabrielcpf2997@gmail.com>
2021-11-04 17:43:39 -0700

    fixed where js file wont load in dist

---
+++ index.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('from js');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsaUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0YXVyYW50X3BhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiYWxlcnQoJ2Zyb20ganMnKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
