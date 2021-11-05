88f3df21802ea0b87ea5956b4b5db9e9797795b6
gabecpf97 <gabrielcpf2997@gmail.com>
2021-11-04 17:43:39 -0700

    fixed where js file wont load in dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    entry: {
+        index: './src/index.js',
+        page_laod: './src/page_load.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Restaurant Page',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: '',
+    },
 };
